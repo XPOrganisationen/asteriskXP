134a05576414a95e33e857918bfdc22c39be74e9
bugintroducer69 <masc1001@stud.ek.dk>
2026-02-27 10:55:27 +0100

    Initial commit (Docker, CI/CD actions)
    A large initial commit, but I:
    * Added a workflow for CI (builds and tests on PRs to main)
    * Added a workflow for deployment (builds, tests, builds a docker container, sends that container to GHCR)
    * Added a Dockerfile to build a container from dependencies, our code and which starts running our application.
    * Added a compose file to not waste time with the terrible Docker CLI. It has a shared network between the database and the application, and also a volume to persist data from the database.
    
    Note: I've set a lot of environment variables in a .env-file, and I'll pass the same file around to all of you, but be VERY careful not to push it to the repository!

---
+++ src/main/java/com/xp/AsteriskXpApplication.java
@@ -0,0 +1,13 @@
+package com.xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteriskXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsteriskXpApplication.class, args);
+    }
+
+}
