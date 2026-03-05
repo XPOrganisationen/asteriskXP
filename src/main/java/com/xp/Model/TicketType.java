c624491381c1fd04a1bd1d7c9b36a5f2ae9513e2
filip-jacobsen <fija0001@stud.kea.dk>
2026-03-05 14:14:37 +0100

    fik merged min egen branch med movieTickets og Seats service og repository som jeg fik lavet

--- src/main/java/com/xp/Model/TicketType.java
+++ src/main/java/com/xp/Model/TicketType.java
@@ -15,32 +15,3 @@ public enum TicketType {
         return price;
     }
 }
-
-//koden nedenunder er kun hvis vi laver en login baseret system hvor når man skriver sin alder, tjekker systemet nummeret -
-// - og tilføjer dig til den ticket type din alder er i (barne billet, voksen billet eller pensionist)
-/*public enum TicketType {
-    CHILD(0, 16, 75),
-    ADULT(17, 64, 140),
-    SENIOR(65, 100, 90 );
-
-    private final int minAge;
-    private final int maxAge;
-    private final double price;
-
-    TicketType(int minAge, int maxAge, double price) {
-        this.minAge = minAge;
-        this.maxAge = maxAge;
-        this.price = price;
-    }
-
-    public boolean matchesAge(int age) {
-        return age >= minAge && age <= maxAge;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-}
-
- */
